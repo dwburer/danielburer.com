93bfbc76491630e7a36908c9d12721bc06873989
Daniel Burer <dburer@morristechnology.com>
2019-01-12 13:06:29 -0500

    cleanup + resume

--- components/nav.js
+++ components/nav.js
@@ -3,12 +3,24 @@ import Link from "next/link";
 export default () => (
     <nav>
         <ul>
-            <li><a href="/index" className="k">home<span>&gt;</span></a></li>
-            <li><a href="/projects" className="k">projects<span>&gt;</span></a></li>
-            <li><a href="#" className="k">résumé<span>&gt;</span></a></li>
-            <Link href="/about">
-                <a href="#" className="k">about<span>&gt;</span></a>
-            </Link>
+            <li>
+                <Link href="/index">
+                    <a href="#" className="k">home<span>&gt;</span></a>
+                </Link>
+            </li>
+            <li>
+                <Link href="/projects">
+                    <a href="#" className="k">projects<span>&gt;</span></a>
+                </Link>
+            </li>
+            <li>
+                <a href="/static/DanielBurer_Resume2019_public.pdf" target="_blank" className="k">résumé<span>&gt;</span></a>
+            </li>
+            <li>
+                <Link href="/about">
+                    <a href="#" className="k">about<span>&gt;</span></a>
+                </Link>
+            </li>
         </ul>
     </nav>
 );
--- layouts/main.js
+++ layouts/main.js
@@ -1,9 +1,9 @@
 import Footer from '../components/footer'
 import Meta from '../components/meta'
 
-import "../scss/styles.scss"
-import "../scss/main.scss"
 import "../scss/normalize.scss"
+import "../scss/main.scss"
+import "../scss/styles.scss"
 
 export default ({ children }) => (
     <div>
